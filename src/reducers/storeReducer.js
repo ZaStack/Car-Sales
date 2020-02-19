f1b22c03b0dcbfa37ff3c1e77b8e17ccb9dd8150
ZaStack <stackz.personal@gmail.com>
2020-02-18 17:23:33 -0800

    Build reducers

--- src/reducers/storeReducer.js
+++ src/reducers/storeReducer.js
@@ -1,4 +1,4 @@
-
+import { ADD_FEATURE, REMOVE_FEATURE } from "../actions/actions"
 
 const initialState = [
     { id: 1, name: 'V-6 engine', price: 1500 },
@@ -9,10 +9,10 @@ const initialState = [
 
 const storeReducer = ( state = initialState, action ) => {
     switch (action.type) {
-        // case ADD_FEATURE:
-        //     return state.filter(feature => feature.id !== action.payload.id);
-        // case REMOVE_FEATURE:
-        //     return [...state, action.payload]
+        case ADD_FEATURE:
+            return state.filter(feature => feature.id !== action.payload.id);
+        case REMOVE_FEATURE:
+            return [...state, action.payload]
         default:
             return state;
     }
